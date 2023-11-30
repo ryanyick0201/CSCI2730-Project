3245ca14b0a37e8b717a608f5fe7762b7c5e54bf
unknown <yickryan34@gmail.com>
2023-11-30 22:04:56 +0800

    Updated file structure and added server-sided script
    - Relocate the contract and related blockchain files to a folder.
    - Create a folder for server side, including server.js and sampleData.json

---
+++ server/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// Initialize Express
+const app = express();
+const port = 3000; // Choose any port you prefer
+
+// MongoDB connection string
+const mongoURI = 'mongodb://127.0.0.1:27017/CSCI2730';
+
+// Define MongoDB Schema
+const flightDelaySchema = new mongoose.Schema({
+    departureDate: Number,
+    departureTime: Number,
+    airline: String,
+    delayTime: Number
+  });
+  
+  // Create MongoDB model
+  const FlightDelay = mongoose.model('flightDelay', flightDelaySchema, 'flightDelay');
+
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    // Define a simple test route
+    app.get('/', (req, res) => {
+      res.send('MongoDB Connection Test Successful!');
+    });
+
+    // Route to retrieve flight delay info based on parameters
+    app.get('/flightDelayInfo', async (req, res) => {
+        try {
+          const { departureDate, departureTime, airline } = req.query;
+  
+          // Query the database based on parameters
+          const flightDelayData = await FlightDelay.find({
+            departureDate: departureDate, departureTime: departureTime, airline: airline
+          });
+  
+          console.log("Data retrieved")
+          res.json(flightDelayData);
+        } catch (error) {
+          console.error('Error retrieving flight delay info:', error);
+          res.status(500).json({ error: 'Internal Server Error' });
+        }
+      });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
